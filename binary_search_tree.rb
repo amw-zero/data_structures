ac50cd7277cb2547130eb002f0b8b758918703c1
Alex Weisberger <alex.m.weisberger@gmail.com>
2016-01-24 09:42:34 -0500

    BST.

---
+++ binary_search_tree.rb
@@ -0,0 +1,81 @@
+class BST
+	attr_accessor :root
+
+	def initialize(root)
+		@root = root
+	end
+
+	def traverse(&blk)
+		_traverse(@root, &blk)
+	end
+
+	def insert(node)
+		raise 'Must initialize BST with root node' if @root.nil?
+
+		_insert(node, @root)
+	end
+
+	def find(v)
+		n = traverse { |n| break n if n == v}
+		n
+	end
+
+	private
+
+	def _insert(node, parent)
+		if node.value < parent.value
+			if parent.left.nil?
+				parent.left = node
+			else
+				_insert(node, parent.left)
+			end
+		else
+			if parent.right.nil?
+				parent.right = node
+			else
+				_insert(node, parent.right)
+			end
+		end
+	end
+
+	def _traverse(node, &blk)
+		return if node.nil?
+
+		_traverse(node.left, &blk)
+
+		yield node
+
+		_traverse(node.right, &blk)
+	end
+end
+
+class Node
+	attr_accessor :value, :left, :right
+
+	def initialize(v)
+		@value = v
+		@left = nil
+		@right = nil
+	end
+
+	def to_s
+		"v = #{@value} | l = #{@left || 'nil' } | r = #{@right || 'nil'}"
+	end
+end
+
+bst = BST.new(Node.new(3))
+bst.insert(Node.new(2))
+bst.insert(Node.new(4))
+bst.insert(Node.new(7))
+bst.insert(Node.new(1))
+bst.insert(Node.new(1))
+bst.insert(Node.new(6))
+bst.insert(Node.new(11))
+bst.insert(Node.new(19))
+bst.insert(Node.new(5))
+
+bst.traverse { |n| puts n.value }
+
+puts bst.find(4)
+
+
